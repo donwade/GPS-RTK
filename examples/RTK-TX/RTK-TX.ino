328fa513922bbd51cb34a7cc7625f167f17db9de
Don Wade <don_wade@rogers.com>
2026-05-06 01:25:44 -0400

    tx and sat ok

--- examples/RTK-TX/RTK-TX.ino
+++ examples/RTK-TX/RTK-TX.ino
@@ -1,51 +1,71 @@
 /*****************************************
-  ESP32 GPS VKEL 9600 Bds
-This version is for T22_v01 20190612 board
-As the power management chipset changed, it
-require the axp20x library that can be found
-https://github.com/lewisxhe/AXP202X_Library
-You must import it as gzip in sketch submenu
-in Arduino IDE
-This way, it is required to power up the GPS
-module, before trying to read it.
-
-Also get TinyGPS++ library from: 
-https://github.com/mikalhart/TinyGPSPlus
-******************************************/
+ * ESP32 GPS VKEL 9600 Bds
+ * This version is for T22_v01 20190612 board
+ * As the power management chipset changed, it
+ * require the axp20x library that can be found
+ * https://github.com/lewisxhe/AXP202X_Library
+ * You must import it as gzip in sketch submenu
+ * in Arduino IDE
+ * This way, it is required to power up the GPS
+ * module, before trying to read it.
+ *
+ * Also get TinyGPS++ library from:
+ * https://github.com/mikalhart/TinyGPSPlus
+ ******************************************/
 
 #include <TinyGPS++.h>
 #include <axp20x.h>
 
 #include <SPI.h>
-#include <Wire.h>  
-#include "SSD1306.h" 
+#include <Wire.h>
+#include "SSD1306.h"
 #include "SX127x_Tx.h"
 
+#define LINE Serial.printf("%s:%d <<< \n", __FUNCTION__, __LINE__);
+
 /*
-#define SCK     5    // GPIO5  -- SX1278's SCK
-#define MISO    19   // GPIO19 -- SX1278's MISO
-#define MOSI    27   // GPIO27 -- SX1278's MOSI
-#define SS      18   // GPIO18 -- SX1278's CS
-#define RST     14   // GPIO14 -- SX1278's RESET
-#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND    433E6
-*/
+ #define SCK     5    // GPIO5  -- SX1278's SCK
+ #define MISO    19   // GPIO19 -- SX1278's MISO
+ #define MOSI    27   // GPIO27 -- SX1278's MOSI
+ #define SS      18   // GPIO18 -- SX1278's CS
+ #define RST     14   // GPIO14 -- SX1278's RESET
+ #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+ #define BAND    433E6
+ */
 
 SSD1306 display(0x3c, 21, 22);
 
 double slowLat = 0.0;
 double slowLng = 0.0;
 
-double	fast_lat,fast_lng;
-double	diff_lat,diff_lng;
+double fast_lat, fast_lng;
+double diff_lat, diff_lng;
 
 uint8_t idx = 0;
 
-#include <esp_task_wdt.h>
 
 TinyGPSPlus gps;
+
 HardwareSerial GPS(1);
 AXP20X_Class axp;
+
+#define HAVE_DOG 0
+#if HAVE_DOG
+    #include <esp_task_wdt.h>
+	#define KICKDOG esp_task_wdt_reset()
+    // Define configuration
+    esp_task_wdt_config_t wdt_config =
+    {
+        .timeout_ms		= 5000,                 // Timeout in milliseconds
+        .idle_core_mask = 0,                    // Do not watch idle tasks
+        .trigger_panic	= false                 //true = restart ) on timeout
+    };
+
+#else
+	#define KICKDOG
+#endif
+
+bool bWDTallowed = HAVE_DOG;
 
 
 //------------------------------------------------------------------
@@ -53,14 +73,16 @@ AXP20X_Class axp;
 
 #define PI 3.14159265358979323846
 #define EARTH_RADIUS_M 6371000.0
-
 // Convert degrees to radians
-double degToRad(double deg) {
-    return (deg * PI / 180.0);
+double degToRad(double deg)
+{
+    return deg * PI / 180.0;
 }
 
+
 // Calculate distance between two points in KM
-double getGPSDistanceM(double lat1, double lon1, double lat2, double lon2) {
+double getGPSDistanceM(double lat1, double lon1, double lat2, double lon2)
+{
     double dLat = degToRad(lat2 - lat1);
     double dLon = degToRad(lon2 - lon1);
 
@@ -68,274 +90,272 @@ double getGPSDistanceM(double lat1, double lon1, double lat2, double lon2) {
     lat2 = degToRad(lat2);
 
     double a = sin(dLat / 2) * sin(dLat / 2) +
-               cos(lat1) * cos(lat2) * 
+               cos(lat1) * cos(lat2) *
                sin(dLon / 2) * sin(dLon / 2);
-    
+
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-    
+
     return EARTH_RADIUS_M * c;
 }
 
+
 //------------------------------------------------------------------
 u_int8_t char_height = 0;
-
 static void setFont(uint8_t size)
 {
-	switch (size)
-	{
-		case 10:
-			display.setFont(ArialMT_Plain_10);
-			char_height = 10;
-		break;
-		
-		case 16:
-			display.setFont(ArialMT_Plain_16);
-			char_height = 16;
-		break;
+    switch (size)
+    {
+    case 10:
+        display.setFont(ArialMT_Plain_10);
+        char_height = 10;
+        break;
 
-		case 24:
-			display.setFont(ArialMT_Plain_24);
-			char_height = 24;
-		break;
-	}
+    case 16:
+        display.setFont(ArialMT_Plain_16);
+        char_height = 16;
+        break;
+
+    case 24:
+        display.setFont(ArialMT_Plain_24);
+        char_height = 24;
+        break;
+    }
 }
 
-int  xprintf(uint8_t lineNo, const char *format, ...) 
+
+int  xprintf(uint8_t lineNo, const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
-	char buffer[100];
-	vsprintf(buffer, format, args);
-	
+    va_list args;
+
+    va_start(args, format);
+    char buffer[100];
+    vsprintf(buffer, format, args);
+
     display.drawString(0, lineNo * char_height, buffer);
-	
-	va_end(args);
-	return 0;
+
+    va_end(args);
+    return 0;
 }
+
 
 //---------------------------------------------------------
-
-void loop(){while(1) delay(-1);};  // keep arduino happy
-
+void loop()
+{
+    while (1)
+        delay(-1);
+};                                 // keep arduino happy
 //---------------------------------------------------------
 void led_display1(void)
 {
-	xprintf(0, "LA=%+9.7f", fast_lat);
-	xprintf(1, "LO=%+9.7f", fast_lng); 
+    xprintf(0, "LA=%+9.7f", fast_lat);
+    xprintf(1, "LO=%+9.7f", fast_lng);
 
 
-	double dist = getGPSDistanceM(fast_lat, fast_lng, slowLat, slowLng);
+    double dist = getGPSDistanceM(fast_lat, fast_lng, slowLat, slowLng);
 
-	xprintf(2, "D=%lf", dist);
-	
+    xprintf(2, "D=%lf", dist);
 
-	//xprintf(2, "SA=%9.7f", slowLat - fast_lat);
-	//xprintf(3, "SO=%9.7f", slowLng - fast_lng);
-	
-	//xprintf(3, "SO=%9.7f", slowLng);
 
-	
-	//xprintf(3, "%d", idx);
-	
-	//xprintf(2, "DIR=%3d %3s", (int)gps.course.deg(), gps.cardinal(gps.course.deg()));
-	//xprintf(3, "M/S=%6.1f", gps.speed.mps());
-	//xprintf(3, "%02d/%02d/%02d", gps.date.day(), gps.date.month(), gps.date.year());
+    //xprintf(2, "SA=%9.7f", slowLat - fast_lat);
+    //xprintf(3, "SO=%9.7f", slowLng - fast_lng);
 
-	
-	//display.display();
-	
+    //xprintf(3, "SO=%9.7f", slowLng);
+
+
+    //xprintf(3, "%d", idx);
+
+    //xprintf(2, "DIR=%3d %3s", (int)gps.course.deg(), gps.cardinal(gps.course.deg()));
+    //xprintf(3, "M/S=%6.1f", gps.speed.mps());
+    //xprintf(3, "%02d/%02d/%02d", gps.date.day(), gps.date.month(), gps.date.year());
+
+
+    //display.display();
+
 }
+
+
 //---------------------------------------------------------
 
 #define NUM_SAMPLES (1 << 8)
 double hist_lat[NUM_SAMPLES];
 double hist_lng[NUM_SAMPLES];
 
-
 void taskGPS(void *not_used)
 {
-	static uint32_t last_tick;
-	static bool bInited = false;
-	
-	while(1)
-	{
-	
-		display.clear();
-		fast_lat = gps.location.lat();
-		fast_lng = gps.location.lng();
+    static uint32_t last_tick;
+    static bool bInited = false;
 
-		if (!fast_lat && !fast_lng)
-		{
-			static uint32_t tick = 0;
-			uint16_t num_satellites = gps.satellites.value();
-			uint16_t quality = gps.hdop.value();
-			display.clear();
+    while (1)
+    {
+        display.clear();
+        fast_lat = gps.location.lat();
+        fast_lng = gps.location.lng();
 
-			xprintf(0, "LA=%+9.7f", fast_lat);
-			xprintf(1, "LN=%+9.7f", fast_lng);
-			
-			xprintf(2, "SA=%02d Q=%d\n", num_satellites, min((uint16_t) 999, quality));
-			xprintf(3, "t=%d\n", tick++);
-					
-			printf("%8d SA=%d QUAL=%d\n", tick, num_satellites, quality);
-			
-		}
-		else
-		{
-		
-			if (!bInited && fast_lat)
-			{
-				for (int j=0; j < NUM_SAMPLES; j++)
-				{
-					hist_lat[j] = fast_lat;
-					hist_lng[j] = fast_lng;
-				}
-				bInited = true;
-			}
-			
-			last_tick = micros();
-			
-			Serial.printf("Latitude  : %lf\n", fast_lat);
-			Serial.printf( "Longitude : %lf\n", fast_lng);
+        if (!fast_lat && !fast_lng)
+        {
+            static uint32_t tick = 0;
+            uint16_t num_satellites = gps.satellites.value();
+            uint16_t quality = gps.hdop.value();
+            display.clear();
 
-			hist_lat[idx] = fast_lat;
-			hist_lng[idx] = fast_lng;
-			
-			idx++;
-			if (idx == NUM_SAMPLES) idx = 0;
-			Serial.printf("idx = %d\n", idx);
+            xprintf(0, "LA=%+9.7f", fast_lat);
+            xprintf(1, "LN=%+9.7f", fast_lng);
 
-			slowLat = 0.0;
-			slowLng = 0.0;
-			
-			for (int i = 0; i < NUM_SAMPLES; i++)
-			{
-				slowLat += hist_lat[i];
-				slowLng += hist_lng[i];
-			}
-			
-			slowLat /= (float) NUM_SAMPLES;
-			slowLng /= (float) NUM_SAMPLES;
-			
-			
-			led_display1();
-	/*
-			Serial.print("Satellites: ");
-			Serial.println(gps.satellites.value());
-			Serial.print("Altitude  : ");
-			Serial.print(gps.altitude.feet() / 3.2808);
-			Serial.println("M");
+            xprintf(2, "SA=%02d Q=%d\n", num_satellites, min((uint16_t)999, quality));
+            xprintf(3, "t=%d\n", tick++);
 
-			Serial.print("Time      : ");
-			Serial.print(gps.time.hour());
-			Serial.print(":");
-			Serial.print(gps.time.minute());
-			Serial.print(":");
-			Serial.println(gps.time.second());
+            printf("%8d SA=%d QUAL=%d\n", tick, num_satellites, quality);
+
+        }
+        else
+        {
+            if (!bInited && fast_lat)
+            {
+                for (int j = 0; j < NUM_SAMPLES; j++)
+                {
+                    hist_lat[j] = fast_lat;
+                    hist_lng[j] = fast_lng;
+                }
+
+                bInited = true;
+            }
+
+            last_tick = micros();
+
+            Serial.printf("Latitude  : %lf\n", fast_lat);
+            Serial.printf("Longitude : %lf\n", fast_lng);
+
+            hist_lat[idx] = fast_lat;
+            hist_lng[idx] = fast_lng;
+
+            idx++;
+
+            if (idx == NUM_SAMPLES)
+                idx = 0;
+
+            Serial.printf("idx = %d\n", idx);
+
+            slowLat = 0.0;
+            slowLng = 0.0;
+
+            for (int i = 0; i < NUM_SAMPLES; i++)
+            {
+                slowLat += hist_lat[i];
+                slowLng += hist_lng[i];
+            }
+
+            slowLat /= (float)NUM_SAMPLES;
+            slowLng /= (float)NUM_SAMPLES;
 
 
-			Serial.print("Speed     : ");
-			Serial.println(gps.speed.kmph()); 
-			Serial.println("**********************");
-	*/		
-			
-			smartDelay(1000);
-			uint32_t diff_time = micros() - last_tick;
-			Serial.printf("report = %d uS\n\n", diff_time);
-			
-			if (diff_time > 1200000 && gps.charsProcessed() < 10)
-			Serial.println(F("No GPS data received: check wiring"));
-		}
+            led_display1();
+            /*
+             *              Serial.print("Satellites: ");
+             *              Serial.println(gps.satellites.value());
+             *              Serial.print("Altitude  : ");
+             *              Serial.print(gps.altitude.feet() / 3.2808);
+             *              Serial.println("M");
+             *
+             *              Serial.print("Time      : ");
+             *              Serial.print(gps.time.hour());
+             *              Serial.print(":");
+             *              Serial.print(gps.time.minute());
+             *              Serial.print(":");
+             *              Serial.println(gps.time.second());
+             *
+             *
+             *              Serial.print("Speed     : ");
+             *              Serial.println(gps.speed.kmph());
+             *              Serial.println("**********************");
+             */
 
-		display.display();
-		smartDelay(1000);
-		//esp_task_wdt_reset();
-				
-	}
+            smartDelay(1000);
+            uint32_t diff_time = micros() - last_tick;
+            Serial.printf("report = %d uS\n\n", diff_time);
+
+            if (diff_time > 1200000 && gps.charsProcessed() < 10)
+                Serial.println(F("No GPS data received: check wiring"));
+        }
+
+        display.display();
+        smartDelay(1000);
+
+		KICKDOG;
+
+    }
 }
 
-//---------------------------------------------------------
 
+//---------------------------------------------------------
 void taskRadio(void *not_used)
 {
-	setup_radio();
+    setup_radio();
 
-	while(1)
-	{
-		loop_radio();
-	}
+    while (1)
+        loop_radio();
 }
 
 //---------------------------------------------------------
-
 static void smartDelay(unsigned long ms)
 {
-  unsigned long start = millis();
-  do
-  {
-    while (GPS.available())
-      esp_task_wdt_reset();      // don't dog in a delay routine duh.
-      gps.encode(GPS.read());
-  } while (millis() - start < ms);
+    unsigned long start = millis();
+    do
+    {
+        if (GPS.available()) gps.encode(GPS.read());
+        // don't dog in a delay routine duh.
+		KICKDOG;
+		yield();
+    }
+    while (millis() - start < ms);
 }
 
 
 //---------------------------------------------------------
-
-
 void setup()
 {
-	Serial.begin(921600);
-	
-	// oled stuff
-	pinMode(16,OUTPUT);
-	digitalWrite(16, LOW);	  // set GPIO16 low to reset OLED
-	delay(50); 
-	digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high?
+    //Serial.begin(921600);
+    Serial.begin(115200);
 
-	// gps power mgt
-	
-	Wire.begin(21, 22);
-	if (!axp.begin(Wire, AXP192_SLAVE_ADDRESS)) {
-	Serial.println("AXP192 Begin PASS");
-	} else {
-	Serial.println("AXP192 Begin FAIL");
-	}
-	axp.setPowerOutPut(AXP192_LDO2, AXP202_ON);
-	axp.setPowerOutPut(AXP192_LDO3, AXP202_ON);
-	axp.setPowerOutPut(AXP192_DCDC2, AXP202_ON);
-	axp.setPowerOutPut(AXP192_EXTEN, AXP202_ON);
-	axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON);
-	GPS.begin(9600, SERIAL_8N1, 34, 12);   //17-TX 18-RX
+    // oled stuff
+    pinMode(16, OUTPUT);
+    digitalWrite(16, LOW);      // set GPIO16 low to reset OLED
+    delay(50);
+    digitalWrite(16, HIGH);     // while OLED is running, must set GPIO16 in high?
 
-	
-	display.init();
-	display.flipScreenVertically();  
-	setFont(16);
-	display.clear();
-	display.setTextAlignment(TEXT_ALIGN_LEFT);
+    // gps power mgt
 
-	xprintf(0, "START");
-	display.display();
-	
+    Wire.begin(21, 22);
 
-	TaskHandle_t hRunRadio, hRunGPS;
+    if (!axp.begin(Wire, AXP192_SLAVE_ADDRESS))
+        Serial.println("AXP192 Begin PASS");
+    else
+        Serial.println("AXP192 Begin FAIL");
 
-	xTaskCreate(taskRadio, "taskRadio", 4096* 4, NULL, 8, &hRunRadio);
-	xTaskCreate(taskGPS, "taskGPS", 4096 * 4, NULL, 8, &hRunGPS);
-	
-	// Define configuration
-	esp_task_wdt_config_t wdt_config =
-	{
-		    .timeout_ms = 5000,        // Timeout in milliseconds
-		    .idle_core_mask = 0,       // Do not watch idle tasks
-		    .trigger_panic = false     //true = restart ) on timeout
-	};
+    axp.setPowerOutPut(AXP192_LDO2, AXP202_ON);
+    axp.setPowerOutPut(AXP192_LDO3, AXP202_ON);
+    axp.setPowerOutPut(AXP192_DCDC2, AXP202_ON);
+    axp.setPowerOutPut(AXP192_EXTEN, AXP202_ON);
+    axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON);
+    GPS.begin(9600, SERIAL_8N1, 34, 12);       //17-TX 18-RX
 
-	esp_task_wdt_reconfigure(&wdt_config); 
-  	esp_task_wdt_add(hRunRadio); 	// Add task hRunGPS to WDT 
-  	esp_task_wdt_add(hRunGPS); 		// Add task hRunGPS to WDT 
-  	
+
+    display.init();
+    display.flipScreenVertically();
+    setFont(16);
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+    xprintf(0, "START");
+    display.display();
+
+
+    TaskHandle_t hRunRadio, hRunGPS;
+
+    xTaskCreate(taskRadio, "taskRadio", 4096 * 4, NULL, 8, &hRunRadio);
+    xTaskCreate(taskGPS, "taskGPS", 4096 * 4, NULL, 8, &hRunGPS);
+
+ //   esp_task_wdt_reconfigure(&wdt_config);
+ //   esp_task_wdt_add(hRunRadio);        // Add task hRunGPS to WDT
+ //   esp_task_wdt_add(hRunGPS);          // Add task hRunGPS to WDT
+
 }
-
-
--- examples/RTK-TX/SX127x_Tx.cpp
+++ examples/RTK-TX/SX127x_Tx.cpp
@@ -1,37 +1,42 @@
 /*
-  RadioLib SX127x Transmit with Interrupts Example
-
-  This example transmits LoRa packets with one second delays
-  between them. Each packet contains up to 255 bytes
-  of data, in the form of:
-  - Arduino String
-  - null-terminated char array (C-string)
-  - arbitrary binary data (byte array)
-
-  Other modules from SX127x/RFM9x family can also be used.
-
-  For default module settings, see the wiki page
-  https://github.com/jgromes/RadioLib/wiki/Default-configuration#sx127xrfm9x---lora-modem
-
-  For full API reference, see the GitHub Pages
-  https://jgromes.github.io/RadioLib/
-*/
+ * RadioLib SX127x Transmit with Interrupts Example
+ *
+ * This example transmits LoRa packets with one second delays
+ * between them. Each packet contains up to 255 bytes
+ * of data, in the form of:
+ * - Arduino String
+ * - null-terminated char array (C-string)
+ * - arbitrary binary data (byte array)
+ *
+ * Other modules from SX127x/RFM9x family can also be used.
+ *
+ * For default module settings, see the wiki page
+ * https://github.com/jgromes/RadioLib/wiki/Default-configuration#sx127xrfm9x---lora-modem
+ *
+ * For full API reference, see the GitHub Pages
+ * https://jgromes.github.io/RadioLib/
+ */
 
 #include "SX127x_Tx.h"
 
 // include the library
 #include <RadioLib.h>
 
+extern bool bWDTallowed;
 #include <esp_task_wdt.h>
+
+
 
 void dogDelay(unsigned long ms)
 {
-  unsigned long start = millis();
-  do
-  {
-      esp_task_wdt_reset();      // don't dog in a delay routine duh.
-      delay(10);
-  } while (millis() - start < ms);
+    unsigned long start = millis();
+
+    do
+    {
+        if(bWDTallowed) esp_task_wdt_reset();    // don't dog in a delay routine duh.
+        delay(10);
+    }
+    while (millis() - start < ms);
 }
 
 
@@ -43,136 +48,135 @@ void dogDelay(unsigned long ms)
 //SX1278 radio = new Module(10, 2, 9, 3);
 
 #ifdef ARDUINO_TTGO_LoRa32_V1
-    // Define pins for TTGO LoRa32 V1
-    #define NSS     18
-    #define RESET   14
-    #define DIO0    26
-    #define DIO1    -1
-    #define BAND    433E6
+// Define pins for TTGO LoRa32 V1
+#define NSS     18
+#define RESET   14
+#define DIO0    26
+#define DIO1    -1
+#define BAND    433E6
 #else
-    #error "setup new params for radio board"
+#error "setup new params for radio board"
 #endif
 
 SX1278 radio = new Module(NSS, DIO0, RESET, DIO1);
 // or detect the pinout automatically using RadioBoards
 // https://github.com/radiolib-org/RadioBoards
 /*
-#define RADIO_BOARD_AUTO
-#include <RadioBoards.h>
-Radio radio = new RadioModule();
-*/
+ #define RADIO_BOARD_AUTO
+ #include <RadioBoards.h>
+ * Radio radio = new RadioModule();
+ */
 
 // save transmission state between loops
 int transmissionState = RADIOLIB_ERR_NONE;
 
 // flag to indicate that a packet was sent
-volatile bool transmittedFlag = false;
+volatile bool bTransmitterAvail = true;
 
-// this function is called when a complete packet
-// is transmitted by the module
-// IMPORTANT: this function MUST be 'void' type
-//            and MUST NOT have any arguments!
-#if defined(ESP8266) || defined(ESP32)
-  ICACHE_RAM_ATTR
-#endif
-void setFlag(void) {
-  // we sent a packet, set the flag
-  transmittedFlag = true;
+ICACHE_RAM_ATTR void setFlag(void)
+{
+    // we sent a packet, set the flag
+    bTransmitterAvail = true;
 }
+
 
 #define FREQ 434.0000
+void setup_radio()
+{
 
-void setup_radio() {
+    // initialize SX1278 with default settings
+    Serial.print("[SX1278] Initializing ... ");
 
-  // initialize SX1278 with default settings
-  Serial.print(F("[SX1278] Initializing ... "));
 
-  
-  int state  = radio.beginFSK(  FREQ, 			//freq 
-  					  		    4.8,			//br
-  					  			5.0,			//freqDev =  
-  					  		    125.0, 			//rxBw = 
-  					  			1,				//txpwr =  
-  					  			16,				//preambleLength =  
-  					  			false);			//enableOOK = 
- 					  
-  //int state = radio.begin();
-  
-  if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("success!"));
-  } else {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-    while (true) { delay(10); }
-  }
+    int state = radio.beginFSK(FREQ,                    //freq
+                               4.8,                     //br
+                               5.0,                     //freqDev =
+                               125.0,                   //rxBw =
+                               1,                       //txpwr =
+                               16,                      //preambleLength =
+                               false);                  //enableOOK =
 
-  // set the function that will be called
-  // when packet transmission is finished
-  radio.setPacketSentAction(setFlag);
-  
-  radio.setOutputPower(2, false);
-  //radio.setFrequency(FREQ);
-  //radio.set
-  
-  // start transmitting the first packet
-  Serial.print(F("[SX1278] Sending first packet ... "));
+    //int state = radio.begin();
 
-  // you can transmit C-string or Arduino string up to
-  // 255 characters long
-  transmissionState = radio.startTransmit("Hello World!");
+    if (state == RADIOLIB_ERR_NONE)
+    {
+        Serial.println("success!");
+    }
+    else
+    {
+        Serial.print("failed, code ");
+        Serial.println(state);
 
-  // you can also transmit byte array up to 255 bytes long
-  /*
-    byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF};
-    transmissionState = radio.startTransmit(byteArr, 8);
-  */
-}
-
-// counter to keep track of transmitted packets
-int count = 0;
-
-void loop_radio() {
-  // check if the previous transmission finished
-  if(transmittedFlag) {
-    // reset flag
-    transmittedFlag = false;
-
-    if (transmissionState == RADIOLIB_ERR_NONE) {
-      // packet was successfully sent
-      Serial.println(F("transmission finished!"));
-
-      // NOTE: when using interrupt-driven transmit method,
-      //       it is not possible to automatically measure
-      //       transmission data rate using getDataRate()
-
-    } else {
-      Serial.print(F("failed, code "));
-      Serial.println(transmissionState);
-
+        while (true)
+            delay(10);
     }
 
-    // clean up after transmission is finished
+    // set the function that will be called
+    // when packet transmission is finished
+    radio.setPacketSentAction(setFlag);
+
+    radio.setOutputPower(2, false);
+    //radio.setFrequency(FREQ);
+    //radio.set
+
+    // start transmitting the first packet
+    Serial.print("[SX1278] Sending first packet ... ");
+
+    // you can transmit C-string or Arduino string up to
+    // 255 characters long
+    transmissionState = radio.startTransmit("Hello World!");
+
+    // you can also transmit byte array up to 255 bytes long
+    /*
+     * byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
+     *                  0x89, 0xAB, 0xCD, 0xEF};
+     * transmissionState = radio.startTransmit(byteArr, 8);
+     */
+}
+
+int txPassPktCtr = 0;
+int txFailPktCtr = 0;
+
+void SendString(char *msg)
+{
+    // check if the previous transmission finished
+    while(!bTransmitterAvail) dogDelay(10);
+    
+    // reset flag
+    bTransmitterAvail = false;
+
+    if (transmissionState == RADIOLIB_ERR_NONE)
+    {
+        // packet was successfully sent
+		txPassPktCtr++;
+        Serial.printf("TX Pass packet count #%d\n", txPassPktCtr);
+    }
+    else
+    {
+        txFailPktCtr++;
+        Serial.printf("TX Fail packet count #%d\n", txFailPktCtr);
+    }
+
+    // ensure last transmission is finished
     // this will ensure transmitter is disabled,
     // RF switch is powered down etc.
     radio.finishTransmit();
 
-    // wait a 30 second before transmitting again
-    dogDelay(30000);
+	Serial.printf("tx packet: %s\n", msg);
+	// now swing back into tx packet mode and send it.
+    transmissionState = radio.startTransmit(msg);
 
-    // send another one
-    Serial.print(F("[SX1278] Sending another packet ... "));
-
-    // you can transmit C-string or Arduino string up to
-    // 255 characters long
-    String str = "Hello World! #" + String(count++);
-    transmissionState = radio.startTransmit(str);
-
-    // you can also transmit byte array up to 255 bytes long
-    /*
-      byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
-                        0x89, 0xAB, 0xCD, 0xEF};
-      transmissionState = radio.startTransmit(byteArr, 8);
-    */
-  }
 }
+
+// counter to keep track of transmitted packets
+void loop_radio()
+{
+	char test[63];
+	sprintf(test, "hello %d", txPassPktCtr);
+	SendString(test);
+	
+	dogDelay(10000);
+}
+
+
+
